6ab05bd74d4c1452f317502ca970dd495c41fc5d
Alejo <alejobustamanter@gmail.com>
2024-06-27 14:29:42 -0500

    acabe la parte donde se muestran las calculadoras es responsive

--- calculadoras.php
+++ calculadoras.php
@@ -2,21 +2,44 @@
   include './includes/templates/header.php';
 ?>
 <body>
-    
+<div class="distribuir margen">
+<div >
+    <h1 class="calch">Nuestras Calculadoras</h1>
+    <p class="info">En nuestro gimnasio, entendemos la importancia de mantener un estilo de vida saludable. Por eso, te invitamos a utilizar nuestras calculadoras especializadas para ayudarte a monitorear y mejorar tu salud:</p>
 
+    <p class="info">Calculadora de Índice de Masa Corporal (IMC)<br>
+    El Índice de Masa Corporal es una medida útil para evaluar si tu peso está dentro de un rango saludable según tu altura. Utiliza nuestra calculadora de IMC para obtener una estimación rápida y precisa de tu estado de peso actual.</p>
+
+    <p class="info">Convertidor de Libras a Kilogramos<br>
+    ¿Necesitas convertir tu peso de libras a kilogramos o viceversa? Nuestra herramienta de conversión te facilitará este proceso de forma sencilla y efectiva.</p>
+
+    <p class="info">Estas herramientas están diseñadas para proporcionarte información útil sobre tu salud y bienestar. ¡No esperes más y úsalas ahora para mantenerte en el camino hacia una vida más saludable!</p>
+</div>
+
+
+    <div class="margen">
         <a class="indice" href="calculadoraimc.php">
         <div class="indice" >
         <h2>Calculadora Imc</h2>
     </div>
     </a>
 
-    <!-- <div class="imag">
+    <div class="imag">
     <img src="src/img/maquina-de-gimnasio.png" class="img2" alt="logo">
-    </div> -->
+    </div>
 
     <a class="lb" href="calculadora.php">
         <div class="lb" >
         <h2>Calculadora Lb a Kg</h2>
-    </div>
-    </a>
+        </a>
+      </div>
+  </div>
+
+    
+    
+</div>
+
+    <footer class="footer">
+      <p>Powered by Ganadero</p>
+    </footer>
 </body>

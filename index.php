876ae30a46ddea871eb02b6b9e22c02adfcec71b
Alejo <alejobustamanter@gmail.com>
2024-06-27 11:52:37 -0500

    hice que cada parte del index fuera un template y los llame

--- index.php
+++ index.php
@@ -19,23 +19,18 @@ $db = conectar(); // Se almacena la conexión en la variable $db
 </div>
 
 <!-- sedes -->
-<div class="sedes">
 
-    <h2>Encuentra la sede <span class="span">más cercana a ti</span></h2>
-    <img class='imagenrock' src="src/img/natalie-dormer-en-gimnasio_2880x1800_xtrafondos.com.jpg" alt="sede">
-    <button class="boton" onclick="window.location.href='sedes.php'">Buscar Sedes</button>
+<?php
 
-</div>
-
+//  include 'includes/templates/sedesm.php';
+?>
 
 
 <!-- quien somos -->
-<section class="seccion">
-<h2>Quienes Somos?<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus corrupti error animi labore esse quos dolor dolores sunt blanditiis possimus aliquid quis deserunt ratione expedita quae recusandae, ex ullam commodi.</p><img  class="logo"  src="src/img/maquina-de-gimnasio.png" alt=""></h2>
-<img class="imagenbox"  src="src/img/pexels-cottonbro-4761661.jpg" alt="somos">
 
-</section>
-
+<?php
+ include 'includes/templates/somos.php';
+?>
 <!-----------------------------
      `    ``               `
       ``  _ `      `       ``
@@ -54,128 +49,27 @@ $db = conectar(); // Se almacena la conexión en la variable $db
 --------------------------------->
 
 <!-- planes -->
-<div class="planes">
-    <h2>Elige Un <span class="span">Plan</span> y Empieza A <span class="span">Entrenar</span></h2>
-    <div class="plan">
-    <?php 
-    // Realizar la consulta para obtener los planes
-    $consulta = obtenerdato();
-    
-    // Verificar si se obtuvieron resultados
-    if ($consulta) {
-        // si se encontraron resultados escribir sobre los resultados y mostrarlos
-        while ($fila = mysqli_fetch_assoc($consulta)) {
 
-            //se hace un div para cada plan
-            echo "<div class='plan-item'>";
+<?php
+ include 'includes/templates/planes.php';
+?>
 
-            //se extrae y muestra el nombre del plan 
-            echo "<h3>" . $fila['nombre'] . "</h3>";
-
-            //se extrae y muestra el precio del plan 
-            echo "<p>Precio: " . $fila['precio'] . "</p>";
-
-            //se extrae y muestra la descripcion del plan del plan 
-            echo "<p>Descripcion:  " . $fila['descripcion'] . "</p>";
-
-            //se cierra el div
-            echo "</div>";
-        }
-
-        //si no se encuentran resultados 
-    } else {
-        echo "No se encontraron planes.";
-    }
-    ?>
-    </div>
-</div>
-
-
-<div class="planes">
-<h2>Conoce Nuestras <span class="span">Promociones</span></h2>
-</div>
-
-<!-- promociones -->
-<div class="promociones">
-<?php 
-    // Realizar la consulta para obtener los planes
-    $consulta = obtenerdatoprom();
-    
-    // Verificar si se obtuvieron resultados
-    if ($consulta) {
-        // si se encontraron resultados escribir sobre los resultados y mostrarlos
-        while ($fila = mysqli_fetch_assoc($consulta)) {
-
-            //se hace un div para cada prom
-            echo "<div class='prom-item'>";
-
-            //se extrae y muestra el nombre del prom
-            echo "<h3>" . $fila['nombre'] . "</h3>";
-
-            //se extrae y muestra el precio del prom
-            echo "<p>Precio: " . $fila['descuento'] . "</p>";
-
-            //se extrae y muestra la descripcion del prom
-            echo "<p>Descripcion:  " . $fila['descripcion'] . "</p>";
-
-            //se cierra el div
-            echo "</div>";
-        }
-
-        //si no se encuentran resultados 
-    } else {
-        echo "No se encontraron promociones.";
-    }
-    ?>
-</div>
 
 
 
-<div class="planes">
-   <h2>Conoce Nuestros <span class="span">Empleados</span></h2>
-</div>
+<!-- promociones -->
+<?php
+ include 'includes/templates/promociones.php';
+?>
+
 
 <!-- seccion de empleados -->
-<div class="empleados"> 
-<?php 
-    // Realizar la consulta para obtener los planes
-    $consulta = obtenerdatoemp();
-    
-    // Verificar si se obtuvieron resultados
-    if ($consulta) {
-        // si se encontraron resultados escribir sobre los resultados y mostrarlos
-        while ($fila = mysqli_fetch_assoc($consulta)) {
 
-            //se hace un div para cada plan
-            echo "<div class='emp-item'>";
+<?php
+ include 'includes/templates/empleados.php';
+?>
 
-            //se extrae y muestra el nombre del plan 
-            echo "<h3>" . $fila['nombre'] . "</h3>";
-
-            echo "<h2>" . $fila['ocupacion'] . "</h2>";
-
-            //se extrae y muestra el precio del plan 
-            echo "<p>Cedula: " . $fila['dni'] . "</p>";
-
-            //se extrae y muestra la descripcion del plan del plan 
-            echo "<p>Correo:  " . $fila['correo'] . "</p>";
-
-            echo "<p>Fecha De Nacimiento:  " . $fila['fecha_nacimiento'] . "</p>";
-
-            //se cierra el div
-            echo "</div>";
-        }
-
-        //si no se encuentran resultados 
-    } else {
-        echo "No se encontraron promociones.";
-    }
-    ?>
-
-</div>
-
-
-
+<!-- footer -->
 <footer class="footmain">
     <p>Hecho Por El Equipo</p>
 </footer>

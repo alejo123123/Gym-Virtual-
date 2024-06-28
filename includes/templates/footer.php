548f7d5c22657140e852dfb63c5ad83e3066dba4
Alejo <alejobustamanter@gmail.com>
2024-06-27 22:20:30 -0500

    el footer para todos

---
+++ includes/templates/footer.php
@@ -0,0 +1,12 @@
+<footer class="foot ">
+    <div class="conoce">
+<nav>
+        <h1 ><a class="titulo" href="index.php"RegisterGym>RegisterGym</a></h1>
+
+    <div class="iconos">
+   <a href="https://github.com/alejo123123/Gym-Pagina-" target="_blank"> <img src="../../src/img/github (1).png" alt=""></a> 
+    </div>
+
+</nav>
+</div>
+</footer>

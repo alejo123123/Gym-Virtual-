14c20074ff6d4877bd31b399b050ee146744c178
Alejo <alejobustamanter@gmail.com>
2024-07-01 12:22:31 -0500

    elimine la funcion de insertar que no la voy a utilizar

--- includes/config/database.php
+++ includes/config/database.php
@@ -134,25 +134,3 @@ function obtenerdatoemp() {
     }
 }
 
-
-function insertar() {
-    global $db;
-    if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["enviar"])) {
-        $nombre = $_POST["nombre"];
-        $correo = $_POST["correo"];
-        $mensaje = $_POST["mensaje"];
-        
-        $sql = $db->query("INSERT INTO usuarios(U_nombre,U_correo,U_mensaje) VALUES('$nombre','$correo','$mensaje')");
-    
-        if ($sql==1) {
-            echo'bien';
-        }
-     else {
-        echo 'error';
-    }
-    
-    } else {
-        echo 'error';
-    }
-}
-?>
